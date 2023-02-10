059458337d6108727cb85daf1849cb029f78e652
Valeriya Belousova <valeriya.belousova.1988@yandex.ru>
2023-02-10 10:45:38 +0300

    lessoncode1

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Введите число");
+string example = Console.ReadLine();
+int a = Convert.ToInt32(example);
+
+Console.WriteLine("Вывод:");
+int result = a * a;
+Console.WriteLine(result);
